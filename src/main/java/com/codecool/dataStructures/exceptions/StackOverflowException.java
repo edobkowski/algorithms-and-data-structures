42e9564d5a6a410fe0499d4a743fd0519c56e80f
edobkowski <edobkowski@gmail.com>
2018-09-14 19:01:50 +0200

    Implement StackOverflowException

---
+++ src/main/java/com/codecool/dataStructures/exceptions/StackOverflowException.java
@@ -0,0 +1,7 @@
+package com.codecool.dataStructures.exceptions;
+
+public class StackOverflowException extends Exception {
+    public StackOverflowException(String message) {
+        super(message);
+    }
+}
