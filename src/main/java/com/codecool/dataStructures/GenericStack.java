41a192f9c0a26c32d60fc562cd14f111ad7d9d09
edobkowski <edobkowski@gmail.com>
2018-09-14 18:42:36 +0200

    Create GenericStack class

---
+++ src/main/java/com/codecool/dataStructures/GenericStack.java
@@ -0,0 +1,41 @@
+package com.codecool.dataStructures;
+
+public class GenericStack <T> {
+
+    private Object[] array;
+    private int size;
+    private int top;
+
+    public GenericStack(int size) {
+        if(size < 1) {
+            throw new IllegalArgumentException();
+        }
+
+        this.size = size;
+        this.array = new Object[size];
+    }
+
+    public T push() {
+        return null;
+    }
+
+    public T pop() {
+        return null;
+    }
+
+    public T peek() {
+        return null;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public int spaceLeft() {
+        return this.size - (this.top + 1);
+    }
+
+    public boolean empty() {
+        return true;
+    }
+}
