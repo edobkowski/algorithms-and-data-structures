af38036b6d6a5b2b19c05fde2cc660aa4a888aa3
edobkowski <edobkowski@gmail.com>
2018-09-14 19:02:01 +0200

    Implement StackUnderflowException

---
+++ src/main/java/com/codecool/dataStructures/exceptions/StackUnderflowException.java
@@ -0,0 +1,7 @@
+package com.codecool.dataStructures.exceptions;
+
+public class StackUnderflowException extends Exception {
+    public StackUnderflowException(String message) {
+        super(message);
+    }
+}
