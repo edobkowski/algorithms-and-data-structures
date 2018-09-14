3fde85523aaf9c7105ea298ab3fe342d24ed47cf
edobkowski <edobkowski@gmail.com>
2018-09-15 00:05:25 +0200

    Add test case: getting value by key

--- src/test/java/com/codecool/dataStructures/HashTableTest.java
+++ src/test/java/com/codecool/dataStructures/HashTableTest.java
@@ -15,4 +15,26 @@ class HashTableTest {
 
         assertEquals(expectedSize, actualSize);
     }
+
+    @Test
+    void addTest_RepeatingKeys() {
+        HashTable<String, Integer> hashTable = new HashTable<>();
+        hashTable.add("two", 1);
+        hashTable.add("two", 3);
+        hashTable.add("two", 2);
+        int expectedSize = 1;
+        int actualSize = hashTable.size();
+
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    void getTest() {
+        HashTable<String, String> hashTable = new HashTable<>();
+        hashTable.add("Ala", "ma kota");
+        String expectedResult = "ma kota";
+        String actualResult = hashTable.get("Ala");
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
