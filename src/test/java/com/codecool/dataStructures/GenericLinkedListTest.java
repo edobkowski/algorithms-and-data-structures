34098ebe0d705abe8751500fea27b747ca4da81b
edobkowski <edobkowski@gmail.com>
2018-09-14 16:16:36 +0200

    Add testcase: append list with correct input

---
+++ src/test/java/com/codecool/dataStructures/GenericLinkedListTest.java
@@ -0,0 +1,22 @@
+package com.codecool.dataStructures;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenericLinkedListTest {
+
+    @Test
+    @DisplayName("Test adding new elements to list with correct input")
+    void addTest() {
+        GenericLinkedList linkedList = new GenericLinkedList();
+        linkedList.add(5);
+        linkedList.add(10);
+        String expectedResult = "5, 10";
+        String actualResult = linkedList.toString();
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
