aae513caa1c867cc93424489db6c41f3086122d2
edobkowski <edobkowski@gmail.com>
2018-09-15 20:07:57 +0200

    Add test case for tinkerTailor algorithm

---
+++ src/test/java/algorithms/TinkerTailorTest.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TinkerTailorTest {
+
+    @Test
+    void tinkerTailor() {
+        int expectedValue = 4;
+        int actualValue = TinkerTailor.enumerate(5, 3);
+    }
+}
