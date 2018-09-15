b568098641b6ece7af90bc9b4ee5e7ffc3b7e9a5
edobkowski <edobkowski@gmail.com>
2018-09-15 17:33:59 +0200

    Refactor: initialize testArray in each test to avoid keeping state between tests

--- src/test/java/algorithms/SortersTest.java
+++ src/test/java/algorithms/SortersTest.java
@@ -8,17 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class SortersTest {
 
     private static final int[] expectedResult = {-5, -4, -3, -2, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 736};
-    private static final int[] unsortedArray  = {736, -2, 0, -5, 1, -2, -3, 8, 9, 4, -4, -1, 2, 5, 3, 6, 7, 10};
-    private static int[] testArray;
-
-    @BeforeEach
-    private void shuffleArray() {
-        testArray = unsortedArray;
-    }
 
 
     @Test
     void bubbleSortTest() {
+        int[] testArray  = {736, -2, 0, -5, 1, -2, -3, 8, 9, 4, -4, -1, 2, 5, 3, 6, 7, 10};
         Sorters.bubbleSort(testArray);
 
         assertArrayEquals(expectedResult, testArray);
@@ -26,6 +20,7 @@ class SortersTest {
 
     @Test
     void insertionSortTest() {
+        int[] testArray  = {736, -2, 0, -5, 1, -2, -3, 8, 9, 4, -4, -1, 2, 5, 3, 6, 7, 10};
         Sorters.insertionSort(testArray);
 
         assertArrayEquals(expectedResult, testArray);
@@ -33,6 +28,7 @@ class SortersTest {
 
     @Test
     void selectionSortTest() {
+        int[] testArray  = {736, -2, 0, -5, 1, -2, -3, 8, 9, 4, -4, -1, 2, 5, 3, 6, 7, 10};
         Sorters.selectionSort(testArray);
 
         assertArrayEquals(expectedResult, testArray);
@@ -40,6 +36,7 @@ class SortersTest {
 
     @Test
     void mergeSortTest() {
+        int[] testArray  = {736, -2, 0, -5, 1, -2, -3, 8, 9, 4, -4, -1, 2, 5, 3, 6, 7, 10};
         Sorters.mergeSort(testArray);
 
         assertArrayEquals(expectedResult, testArray);
@@ -47,6 +44,7 @@ class SortersTest {
 
     @Test
     void quickSortTest() {
+        int[] testArray  = {736, -2, 0, -5, 1, -2, -3, 8, 9, 4, -4, -1, 2, 5, 3, 6, 7, 10};
         Sorters.quickSort(testArray);
 
         assertArrayEquals(expectedResult, testArray);
